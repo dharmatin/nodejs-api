70471b3a1510eb9856fd3654fa8fc7c002506409
Test <test@gmail.com>
2018-01-16 09:57:31 +0700

    Libs path

---
+++ libs/logger.js
@@ -0,0 +1,34 @@
+import config from "../common/config";
+import winston from "winston";
+import path from "path";
+import fs from "fs";
+import dateFormat from "dateformat";
+
+class Logger {
+  constructor() {
+    const filename = `/media/dharmatin/Data/nodefier/node-api/logs/test-${dateFormat(new Date(), "yyyymmdd")}.log`;
+
+    return new (winston.Logger)({
+      // level: 'debug',
+      transports: [
+        new (winston.transports.File)({ 
+          filename: filename, 
+          json: false,
+          timestamp: function() {
+            return dateFormat(new Date(), "yyyy/mm/dd hh:MM:ss");
+          },
+          formatter: function(options) {
+            return `${options.timestamp()} [${options.level}]: [${options.message}]`
+          }
+        })
+      ]
+    }); 
+  }
+
+  static write(level, message) {
+    const logger  = new Logger();
+    logger.log(level, message);
+  }
+}
+
+export default Logger;
