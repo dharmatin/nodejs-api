24db78ca2a6019827c0737dfe9702f2995c96e2e
Test <test@gmail.com>
2018-01-16 09:56:31 +0700

    Initial commit app module

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import accessLog from "morgan";
+import path from "path";
+import fs from "fs";
+import config from "./common/config.json";
+import dateFormat from "dateformat";
+import logger from "./libs/logger";
+const app = express();
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, `${config.development.accessLog}/access-${dateFormat(new Date(), "yyyymmdd")}.log`), 
+  {flags: 'a'}
+);
+
+//set date token to localtime
+accessLog.token('date', function() {
+  var p = new Date().toString().replace(/[A-Z]{3}\+/,'+').split(/ /);
+  return( p[2]+'/'+p[1]+'/'+p[3]+':'+p[4]+' '+p[5] );
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(accessLog('combined', {stream: accessLogStream}));
+
+export default app;
+
+
