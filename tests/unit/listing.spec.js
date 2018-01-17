55bb56fbbe32be69da9492a3d9284fcf6af4b81c
dharmatin <dharmatin@gmail.com>
2018-01-17 18:01:28 +0700

    add git ignore

--- tests/unit/listing.spec.js
+++ tests/unit/listing.spec.js
@@ -19,12 +19,12 @@ describe("Listing DAO", () => {
   describe("#all", () => {
     it("Should be return all listing from solr", () => {
       const result = listingDao.search();
-      expect(result).to.eventually.have.property("responseHeader").and.to.have.property("status").eventually.equal(0);
+      return expect(result).to.eventually.have.property("responseHeader").and.to.have.property("status").eventually.equal(0);
     });
 
     it("Should be rejected if the status header not equal 0", () => {
       const result = listingDao.search();
-      expect(result).to.eventually.have.property("responseHeader").and.to.have.property("status").equal(0).to.be.rejected;
+      return expect(result).to.eventually.have.property("responseHeader").and.to.have.property("status").equal(1).to.be.rejected;
     })
   })
 })
