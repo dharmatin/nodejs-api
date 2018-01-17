76b57a1637a66ad5fe1fd74ee41bcf6fdb33c480
Test <test@gmail.com>
2018-01-17 15:30:50 +0700

    Listing DAO

---
+++ dao/Solr.js
@@ -0,0 +1,24 @@
+import solr from "solr-client";
+import config from "./SolrConfig.json";
+import bluebird from "bluebird";
+
+export default class SolrClient {
+
+  constructor(core) {
+    this.client = bluebird.promisifyAll(solr.createClient({
+      host: config[process.env.NODE_ENV].host,
+      port: config[process.env.NODE_ENV].port,
+      path: config[process.env.NODE_ENV].path,
+      core: core
+    }));
+  }
+
+  createQuery() {
+    return this.client.createQuery();
+  }
+
+  searchAsync(query) {
+    return this.client.searchAsync(query);
+  }
+
+}
---
+++ dao/solr/listing.js
@@ -0,0 +1,17 @@
+import Solr from "../Solr";
+
+const CORE_LISTING = "listing";
+
+export class Listing extends Solr {
+
+  constructor() {
+    super(CORE_LISTING);
+  }
+  async search(id) {
+    const qCondition = id ? `id:${id}`: '*:*';
+    const query = this.createQuery().q(qCondition).start(0).rows(10);
+    return await this.searchAsync(query);
+  }
+}
+
+export default Listing = new Listing();
